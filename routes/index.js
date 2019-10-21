1668c0f84c7f3430843d589cd7612e93ca5b676a
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-10-21 19:44:52 -0400

    added queries

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,28 @@ router.get('/', (req, res) => {
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
-        console.log(result); // should see objects wrapped in an array
+        //console.log(result); // should see objects wrapped in an array
 
         // render the home view with dynamic data
         res.render('home', { people: result });
     })
 })
+
+router.get('/prof:id', (req, res)=>{ //":__" is a dynamic placeholder
+    console.log('Hit dynamic route.')
+    console.log(req.params.id)
+
+    //sql query
+    let query = `SELECT * FROM tbl_card WHERE profID="${req.params.id}"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        console.log(result); // should see objects wrapped in an array
+
+        // render the home view with dynamic data
+        //res.render('prof:id', { people: result });
+    })
+}) 
 
 module.exports = router;
