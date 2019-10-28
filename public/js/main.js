d100de3bbcb169651c198d4150bcfe38c9e9765e
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-10-28 19:40:38 -0400

    fuck this shit

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,59 @@
 // this is a partially revealing module pattern - just a variation on what we've already done
-
 const myVM = (() => {
     // get the user buttons and fire off an async DB query with Fetch
 
+    let userButtons = document.querySelectorAll('.u-link'),
+        lightbox = document.querySelector('.lightbox');
+
+    function renderSocialMedia(social){
+        return `<ul class='ul-social>
+                    ${social.map(item => `<li>${item}</li>`).join('')}
+                </ul>`
+    }
+
+    function parseUserData(person){
+        let targetDiv = document.querySelector('.lb-content'),
+            targetImg = lightbox.querySelector('img');
+
+        let bioContent = `
+        <p>${person.bio}</p>
+        <h4>Social Media:</h4>`
+        ${renderSocialMedia(person.SocialMedia)};
+
+        console.log(bioContent);
+
+        targetDiv.innerHTML = bioContent;
+        targetImg.src = person.imgsrc;
+
+        lightbox.classList.add('show-lb'); 
+    }
+
+    function getUserData(e){
+        //let userID = e.getAttribute('href');
+        e.preventDefault();
+
+        let url = `/${this.getAttribute('href')}`; // 1,2 or 3
+        let imgSrc = this.previousElementSibling.getAttribute('src');
+
+        fetch(url)
+            .then(res => res.json()) 
+            .then(data =>{
+                console.log(data)
+
+                data.imgsrc = imgSrc;
+
+                parseUserData(data)
+            })
+            .catch((err)=>{
+                console.log(err)
+            }) //res.json(result)
+    }
+
+    userButtons.forEach(button => button.addEventListener('click', getUserData));
+
+    lightbox.querySelector('.close').addEventListener('click', function(){
+        console.log('closing lb');
+        lightbox.classList.remove('show-lb');
+    });
 })();
+
--- routes/index.js
+++ routes/index.js
@@ -7,32 +7,46 @@ router.get('/', (req, res) => {
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the main route');
 
-    let query = "SELECT ID, avatar, Name, Logo, JobTitle FROM tbl_card";
-
-    sql.query(query, (err, result) => {
-        if (err) { throw err; console.log(err); }
-
-        //console.log(result); // should see objects wrapped in an array
-
-        // render the home view with dynamic data
-        res.render('home', { people: result });
-    })
-})
-
-router.get('/prof:id', (req, res)=>{ //":__" is a dynamic placeholder
-    console.log('Hit dynamic route.')
-    console.log(req.params.id)
-
-    //sql query
-    let query = `SELECT * FROM tbl_card WHERE profID="${req.params.id}"`;
+    let query = "SELECT * FROM tbl_card";
 
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
         console.log(result); // should see objects wrapped in an array
 
+        result[0].SocialMedia = result[0].SocialMedia.split(',').map(function(item){
+            item = item.trim();
+
+            return item;
+        });
+        console.log('after spliiting: ', result[0]);
+
         // render the home view with dynamic data
-        //res.render('prof:id', { people: result });
+        res.render('home', { person: result });
+    })
+})
+
+router.get('/:id', (req, res)=>{ //":__" is a dynamic placeholder ***** important
+    console.log('Hit dynamic route.')
+    console.log(req.params.id)
+
+    //sql query
+    let query = `SELECT * FROM tbl_card WHERE ID="${req.params.id}"`; //profID
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        console.log(result); // should see objects wrapped in an array
+
+        result[0].SocialMedia = result[0].SocialMedia.split(',').map(function(item){
+            item = item.trim();
+
+            return item;
+        });
+        //console.log('after spliiting: ', result[0]);
+
+        // rendering results from click (see main.js)
+        res.json(result);
     })
 }) 
 
